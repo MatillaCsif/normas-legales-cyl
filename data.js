a3144cd9387906ea911660d34e48214e81ff7b27
MatillaCsif <matillacsif@gmail.com>
2025-07-10 23:52:32 +0200

    Add files via upload

--- data.js
+++ data.js
@@ -1,50 +1,283 @@
 const documentos = [
   {
-    "titulo": "Decreto 59/2013 – Texto consolidado",
-    "tipo": "Decreto",
-    "fecha": "2016-03-18",
+    "titulo": "ACUERDO_COMPL. ESPEC. 33 AGENTES MEDIOAMBIENTALES",
+    "tipo": "Acuerdo",
+    "fecha": "",
     "etiquetas": [
-      "Decreto 59/2013",
-      "permiso",
-      "lactancia",
-      "conciliación",
-      "jornada"
+      "agentes medioambientales",
+      "complemento específico",
+      "acuerdo"
     ],
-    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Decreto%2B59%2B2013%2Btexto%2Bconsolidado%2Ba%2B18_03_2016.pdf"
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/ACUERDO_COMPL.+ESPEC.+33+AGENTES+MEDIOAMBIENTALES.pdf"
   },
   {
-    "titulo": "Convenio Colectivo 21-06-2023",
+    "titulo": "Acta (modificada) CP 14-6-2024",
+    "tipo": "Acta",
+    "fecha": "",
+    "etiquetas": [
+      "CP",
+      "modificada",
+      "complemento"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Acta%20(modificada)+CP+14-6-2024.pdf"
+  },
+  {
+    "titulo": "Acta (modificada) CP 20-03-2024",
+    "tipo": "Acta",
+    "fecha": "",
+    "etiquetas": [
+      "CP",
+      "modificada",
+      "complemento"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Acta%20(modificada)+CP+20-03-2024.pdf"
+  },
+  {
+    "titulo": "Acta (modificada) CP 25-9-2023,0",
+    "tipo": "Acta",
+    "fecha": "",
+    "etiquetas": [
+      "CP",
+      "modificada",
+      "complemento"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Acta%20(modificada)+CP+25-9-2023,0.pdf"
+  },
+  {
+    "titulo": "Acta (modificada) CP 26-10-2023",
+    "tipo": "Acta",
+    "fecha": "",
+    "etiquetas": [
+      "CP",
+      "modificada",
+      "complemento"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Acta%20(modificada)+CP+26-10-2023.pdf"
+  },
+  {
+    "titulo": "CONVENIO COLECTIVO 21-06-2023",
     "tipo": "Convenio",
-    "fecha": "2023-06-21",
+    "fecha": "",
     "etiquetas": [
       "convenio colectivo",
-      "vacaciones",
-      "retribuciones",
-      "contratación"
+      "contratación",
+      "vacaciones"
     ],
-    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/CONVENIO%2BCOLECTIVO%2B21-06-2023.pdf"
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/CONVENIO%20COLECTIVO%2021-06-2023.pdf"
   },
   {
-    "titulo": "Acta modificada CP 14-6-2024",
-    "tipo": "Acta",
-    "fecha": "2024-06-14",
+    "titulo": "DGFP ACLARACIÓN DEL CRITERIO DERIVADO DE LA CONSULTA SOBRE EL PERMISO POR ENFERMEDAD GRAVE DE FAMILIAR 27_03_2017",
+    "tipo": "Informe",
+    "fecha": "",
     "etiquetas": [
-      "acuerdo",
-      "complemento",
-      "horario"
+      "enfermedad grave",
+      "permiso",
+      "familiar"
     ],
-    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Acta%20(modificada)%2BCP%2B14-6-2024.pdf"
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20ACLARACIÓN%20DEL%20CRITERIO%20DERIVADO%20DE%20LA%20CONSULTA%20SOBRE%20EL%20PERMISO%20POR%20ENFERMEDAD%20GRAVE%20DE%20FAMILIAR%2027_03_2017.pdf"
   },
   {
-    "titulo": "Estatuto de los Trabajadores (consolidado)",
+    "titulo": "DGFP ARMONIZACIÓN DE PERMISOS Y LICENCIAS 15_03_2017",
+    "tipo": "Instrucción",
+    "fecha": "",
+    "etiquetas": [
+      "permisos",
+      "licencias"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20ARMONIZACIÓN%20DE%20PERMISOS%20Y%20LICENCIAS%2015_03_2017.pdf"
+  },
+  {
+    "titulo": "DGFP COMPATIBILIDAD ENTRE EL PERMISO POR NACIMIENTO ADOPCIÓN O ACOGIMIENTO PERMANENTE Y LA SUSPENSION DEL CONTRATO POR PATERNIDAD PREVISTOS PARA EL PERSONAL LABORAL 13_07_2017",
+    "tipo": "Informe",
+    "fecha": "",
+    "etiquetas": [
+      "permiso",
+      "nacimiento",
+      "adopción",
+      "paternidad"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20COMPATIBILIDAD%20ENTRE%20EL%20PERMISO%20POR%20NACIMIENTO%20ADOPCIÓN%20O%20ACOGIMIENTO%20PERMANENTE%20Y%20LA%20SUSPENSION%20DEL%20CONTRATO%20POR%20PATERNIDAD%20PREVISTOS%20PARA%20EL%20PERSONAL%20LABORAL%2013_07_2017.pdf"
+  },
+  {
+    "titulo": "DGFP CONSULTA SOBRE EL PERMISO POR ENFERMEDAD GRAVE DE FAMILIAR 15_03_2017",
+    "tipo": "Informe",
+    "fecha": "",
+    "etiquetas": [
+      "enfermedad grave",
+      "permiso",
+      "familiar"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20CONSULTA%20SOBRE%20EL%20PERMISO%20POR%20ENFERMEDAD%20GRAVE%20DE%20FAMILIAR%2015_03_2017.pdf"
+  },
+  {
+    "titulo": "DGFP CONSULTA SOBRE EL PERMISO POR GUARDA LEGAL 25_09_2018",
+    "tipo": "Informe",
+    "fecha": "",
+    "etiquetas": [
+      "guarda legal",
+      "permiso"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20CONSULTA%20SOBRE%20EL%20PERMISO%20POR%20GUARDA%20LEGAL%2025_09_2018.pdf"
+  },
+  {
+    "titulo": "DGFP Criterio dias adicionales vacaciones asuntos particulares  jubilacion parcial acumulada 12_05_2017",
+    "tipo": "Informe",
+    "fecha": "",
+    "etiquetas": [
+      "vacaciones",
+      "asuntos particulares",
+      "jubilación parcial"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20Criterio%20dias%20adicionales%20vacaciones%20asuntos%20particulares%20%20jubilacion%20parcial%20acumulada%2012_05_2017.pdf"
+  },
+  {
+    "titulo": "DGFP INSTRUCCIÓN licencia por asuntos propios o de la licencia sin retribución 07_11_2017",
+    "tipo": "Instrucción",
+    "fecha": "",
+    "etiquetas": [
+      "asuntos propios",
+      "licencia sin retribución"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20INSTRUCCIÓN%20licencia%20por%20asuntos%20propios%20o%20de%20la%20licencia%20sin%20retribución%2007_11_2017.pdf"
+  },
+  {
+    "titulo": "DGFP Instrucción colaboración tribunales13_07_2017",
+    "tipo": "Instrucción",
+    "fecha": "",
+    "etiquetas": [
+      "colaboración",
+      "tribunales"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20Instrucción%20colaboración%20tribunales13_07_2017.pdf"
+  },
+  {
+    "titulo": "DGFP PERMISO DE LACTANCIA EN CASO DE PARTO MÚLTIPLE 16_05_2017",
+    "tipo": "Informe",
+    "fecha": "",
+    "etiquetas": [
+      "permiso",
+      "lactancia",
+      "parto múltiple"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20PERMISO%20DE%20LACTANCIA%20EN%20CASO%20DE%20PARTO%20MÚLTIPLE%2016_05_2017.pdf"
+  },
+  {
+    "titulo": "DGFP REDUCCIÓN DE JORNADA POR ENFERMEDAD MUY GRAVE DE FAMILIAR DE PRIMER 13_07_2017",
+    "tipo": "Informe",
+    "fecha": "",
+    "etiquetas": [
+      "reducción de jornada",
+      "enfermedad muy grave"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20REDUCCIÓN%20DE%20JORNADA%20POR%20ENFERMEDAD%20MUY%20GRAVE%20DE%20FAMILIAR%20DE%20PRIMER%2013_07_2017.pdf"
+  },
+  {
+    "titulo": "DGFP complementación de la prestación económica en situaciones de incapacidad temporal y para la justificación de ausencias por enfermedad 04_09_2018",
+    "tipo": "Informe",
+    "fecha": "",
+    "etiquetas": [
+      "incapacidad temporal",
+      "ausencias",
+      "enfermedad"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP%20complementación%20de%20la%20prestación%20económica%20en%20situaciones%20de%20incapacidad%20temporal%20y%20para%20la%20justificación%20de%20ausencias%20por%20enfermedad%2004_09_2018.pdf"
+  },
+  {
+    "titulo": "DGFP_PLAZO_MAXIMO_VACACIONES_IT_PERMISOS_PATERNIDAD_MATERNIDAD_RIESGO_EMBARAZO_LACTANCIA_2018_01_31",
+    "tipo": "Instrucción",
+    "fecha": "",
+    "etiquetas": [
+      "vacaciones",
+      "maternidad",
+      "lactancia"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/DGFP_PLAZO_MAXIMO_VACACIONES_IT_PERMISOS_PATERNIDAD_MATERNIDAD_RIESGO_EMBARAZO_LACTANCIA_2018_01_31.pdf"
+  },
+  {
+    "titulo": "Decreto 136_2002, de 26 de diciembre, de la Junta de Castilla y León,  Reglamento regula Escala de Agentes Medioambientales del Cuerpo de Ayudantes Facultativos BOCYL-D-27122002-4",
+    "tipo": "Decreto",
+    "fecha": "",
+    "etiquetas": [
+      "agentes medioambientales",
+      "reglamento"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Decreto%20136_2002,%20de%2026%20de%20diciembre,%20de%20la%20Junta%20de%20Castilla%20y%20León,%20%20Reglamento%20regula%20Escala%20de%20Agentes%20Medioambientales%20del%20Cuerpo%20de%20Ayudantes%20Facultativos%20BOCYL-D-27122002-4.pdf"
+  },
+  {
+    "titulo": "Decreto 59 2013 texto consolidado a 18_03_2016",
+    "tipo": "Decreto",
+    "fecha": "",
+    "etiquetas": [
+      "permiso",
+      "conciliación",
+      "lactancia",
+      "jornada"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Decreto%2059%202013%20texto%20consolidado%20a%2018_03_2016.pdf"
+  },
+  {
+    "titulo": "Decreto-ley 3_2018 se habilita el incremento de las retribuciones abono IT del personal al servicio del sector publico-consolidado",
+    "tipo": "Decreto-ley",
+    "fecha": "",
+    "etiquetas": [
+      "incremento retribuciones",
+      "IT",
+      "sector público"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Decreto-ley%203_2018%20se%20habilita%20el%20incremento%20de%20las%20retribuciones%20abono%20IT%20del%20personal%20al%20servicio%20del%20sector%20publico-consolidado.pdf"
+  },
+  {
+    "titulo": "Estatuto de los trabajadores a 11062025.php",
     "tipo": "Estatuto",
-    "fecha": "2025-06-11",
+    "fecha": "",
     "etiquetas": [
       "estatuto",
-      "derechos laborales",
-      "trabajo efectivo",
-      "salario"
+      "trabajadores",
+      "derechos laborales"
     ],
     "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Estatuto%20de%20los%20trabajadores%20a%2011062025.php.pdf"
+  },
+  {
+    "titulo": "Instrucción VCFPyGA complementación IT",
+    "tipo": "Instrucción",
+    "fecha": "",
+    "etiquetas": [
+      "complementación",
+      "IT"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Instrucción%20VCFPyGA%20complementación%20IT.pdf"
+  },
+  {
+    "titulo": "Ley 7-2005-consolidado",
+    "tipo": "Ley",
+    "fecha": "",
+    "etiquetas": [
+      "ley 7/2005",
+      "personal funcionario",
+      "autonómica"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Ley%207-2005-consolidado.pdf"
+  },
+  {
+    "titulo": "Orden HAC-2-2013 a 1206202522",
+    "tipo": "Orden",
+    "fecha": "",
+    "etiquetas": [
+      "orden",
+      "hacienda",
+      "personal"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/Orden%20HAC-2-2013%20a%201206202522.pdf"
+  },
+  {
+    "titulo": "TRLEBEP a 11062025",
+    "tipo": "Estatuto",
+    "fecha": "",
+    "etiquetas": [
+      "TRLEBEP",
+      "empleo público",
+      "funcionarios"
+    ],
+    "url": "https://github.com/MatillaCsif/normas-legales-cyl/raw/main/TRLEBEP%20a%2011062025.pdf"
   }
 ];
